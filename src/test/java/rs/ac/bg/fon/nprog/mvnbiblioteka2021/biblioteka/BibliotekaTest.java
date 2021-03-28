bcf87eb30e4f692a4881e57bc7873122540fd49a
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-28 14:37:03 +0200

    Prepravljen dependency za JUnit

--- src/test/java/rs/ac/bg/fon/nprog/mvnbiblioteka2021/biblioteka/BibliotekaTest.java
+++ src/test/java/rs/ac/bg/fon/nprog/mvnbiblioteka2021/biblioteka/BibliotekaTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BibliotekaTestPotentialFix {
+public class BibliotekaTest {
 	
 	public Biblioteka biblioteka;
 
